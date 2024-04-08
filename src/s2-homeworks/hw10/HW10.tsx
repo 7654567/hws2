820ead57a812b4cdfd73c9602425159f1f860e7e
7654567 <khiblinsergiy@gmail.com>
2024-04-08 15:09:43 +0200

    s4 hw2

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
+// import {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
@@ -15,11 +15,16 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    // const isLoading = false
+    const isLoading = useSelector((state:any) => state.loading.isLoading)
+    const dispatch = useDispatch()
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
+        console.log(isLoading)
+        dispatch(loadingAC(true))
+        setTimeout(()=>{
+            dispatch(loadingAC(false))
+        }, 1500)
         // setTimeout
     }
 
